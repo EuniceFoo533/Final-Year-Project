694421d200564c0adcb836d90810bab80f95e320
EuniceFoo533 <b032110050@student.utem.edu.my>
2023-06-12 18:55:50 +0800

    Final Year Project

---
+++ app/src/main/java/com/example/myapplication/Vehicles.kt
@@ -0,0 +1,3 @@
+package com.example.myapplication
+
+data class Vehicles(val vehicle_number:String, val vehicle_type:String, val vehicle_name:String, val userID:String)
--- app/src/main/java/com/example/myapplication/fragments/AddVehicleFragment.kt
+++ app/src/main/java/com/example/myapplication/fragments/AddVehicleFragment.kt
@@ -5,56 +5,93 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.Toast
 import com.example.myapplication.R
+import com.example.myapplication.Users
+import com.example.myapplication.Vehicles
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AddVehicleFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AddVehicleFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_vehicle, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddVehicleFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AddVehicleFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val  type = resources.getStringArray(R.array.Vehicle)
+
+        firebaseAuth =FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
+
+        var currentUser = firebaseAuth.currentUser
+
+        var spinner = getView()?.findViewById<Spinner>(R.id.edtType)
+
+        var vNumber = getView()?.findViewById<EditText>(R.id.edtNumber)
+        var vName = getView()?.findViewById<EditText>(R.id.edtName)
+        var btnAdd = getView()?.findViewById<Button>(R.id.buttonAdd)
+
+        if(spinner!= null){
+            val adapter = ArrayAdapter(requireContext(),android.R.layout.simple_spinner_dropdown_item,type)
+            spinner.adapter = adapter
+        }
+
+        btnAdd?.setOnClickListener {
+            val selectedType = spinner?.selectedItem.toString()
+            val number = vNumber?.text.toString()
+            val name = vName?.text.toString()
+
+
+            if(number.isEmpty())
+            {
+                Toast.makeText(context,"Please Enter Your Vehicle Number.",Toast.LENGTH_SHORT).show()
             }
+
+            else if(name.isEmpty())
+            {
+                Toast.makeText(context,"Please Enter Your Vehicle Name.",Toast.LENGTH_SHORT).show()
+            }
+
+            else
+            {
+                val vehicle = Vehicles(number,selectedType,name,currentUser!!.uid)
+
+                // Add a new document with a generated ID
+                db.collection("vehicles")
+                    .add(vehicle)
+                    .addOnSuccessListener { documentReference ->
+                        Toast.makeText(context,"Your vehicle has been successfully added.",Toast.LENGTH_SHORT).show()
+
+                    }
+                    .addOnFailureListener { e ->
+                        Toast.makeText(context,"Failed to Add!",Toast.LENGTH_SHORT).show()
+
+                    }
+            }
+
+
+
+        }
+
+
+
+
+
+
+
+
     }
 }
