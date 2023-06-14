32d871ab565be1ed44168825b4a738549b50da29
EuniceFoo533 <b032110050@student.utem.edu.my>
2023-06-14 18:58:22 +0800

    Final Year Project

--- app/src/main/java/com/example/myapplication/fragments/SeasonalFragment.kt
+++ app/src/main/java/com/example/myapplication/fragments/SeasonalFragment.kt
@@ -1,20 +1,35 @@
 package com.example.myapplication.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 import android.widget.Toast
 import com.example.myapplication.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
 
 // TODO: Rename parameter arguments, choose names that match
 
 
 class SeasonalFragment : Fragment() {
 
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
+    private lateinit var carsRef: CollectionReference
 
+    private fun populateSpinner(itemList: List<String>) {
+        val spinner = getView()?.findViewById<Spinner>(R.id.spinnerCar)
+        val adapter: ArrayAdapter<String> = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, itemList)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner?.adapter = adapter
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -24,6 +39,32 @@ class SeasonalFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        firebaseAuth =FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
+        carsRef = db.collection("vehicles")
+
+        var currentUser = firebaseAuth.currentUser
+
+        // Retrieve data into spinner
+        carsRef
+            .whereEqualTo("userID",currentUser!!.uid)
+            .get().addOnSuccessListener { documents ->
+                if(documents!=null && !documents.isEmpty)
+                {
+                    val itemList = mutableListOf<String>()
+                    for(document in documents){
+                        val item = document.getString("vehicle_number")
+                        if(item !=null){
+                            itemList.add(item)
+                        }
+                        populateSpinner(itemList)
+                    }
+                }
+
+            }.addOnFailureListener{ exception ->
+                Log.e("Firestore", "Error getting documents: ", exception)
+            }
 
 
         val btnHourly = getView()?.findViewById<Button>(R.id.btnHourly)
